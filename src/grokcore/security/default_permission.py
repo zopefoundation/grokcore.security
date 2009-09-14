f1988d3fd15746ec4f6e2a7e2316eab8912a5cea
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2009-09-14 14:44:59 +0000

    Change the decision to use grok.View as the default permission for views and use the more "standard" zope.View permission instead.

--- src/grokcore/security/default_permission.py
+++
@@ -1,8 +0,0 @@
-from grokcore.security import Permission
-from grokcore.component import name
-
-
-class DefaultPermission(Permission):
-    """Default permission set as a fallback for unprotected classes.
-    """
-    name("grok.View")
