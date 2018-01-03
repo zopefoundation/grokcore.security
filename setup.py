863370d125f2545a341faf603e98d9091806e2d2
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2018-01-03 15:00:40 +0100

    Preparing release 1.7

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ tests_require = [
 
 setup(
     name='grokcore.security',
-    version='1.6.4.dev0',
+    version='1.7',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
